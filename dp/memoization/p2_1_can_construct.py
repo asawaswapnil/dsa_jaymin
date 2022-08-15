f9e3611450453bc0e3c843e35edbf4a4008e0c36
Jaymin Desai <jddesai2@ncsu.edu>
2022-08-15 13:51:16 -0700

    Added a memoized solution for can_construct using indices

--- dp/memoization/p2_1_can_construct.py
+++ dp/memoization/p2_1_can_construct.py
@@ -35,12 +35,32 @@ def can_construct_memo(target: str, word_list: List[str], memo: Dict[str, bool])
         return True
     for word in word_list:
         if target.startswith(word):
-            result = can_construct_memo(target[len(word):], word_list, memo)
-            if result:
+            if can_construct_memo(target[len(word):], word_list, memo):
                 memo[target] = True
                 return memo[target]
     memo[target] = False
     return memo[target]
+
+
+def can_construct_memo_idx(idx: int, target: str, word_list: List[str], memo: Dict[str, bool]) -> bool:
+    """
+    Memoized solution using index pointer – time = num of nodes in the optimized tree, space = height of tree
+    If `n` is the length of word list and `m` is the length of the target word
+    Time: O(m^2 x n); in worst case, for every character in target word, we have to look at the entire word list
+        (so O(m x n)) and apart from that, the extra O(m) is for prefix checking + slicing inside the loop
+    Space: O(m); stack depth is O(m)
+    """
+    if idx in memo:
+        return memo[idx]
+    if idx == len(target):
+        return True
+    for word in word_list:
+        if target[idx:].startswith(word):
+            if can_construct_memo_idx(idx + len(word), target, word_list, memo):
+                memo[idx] = True
+                return memo[idx]
+    memo[idx] = False
+    return memo[idx]
 
 
 if __name__ == '__main__':
@@ -51,3 +71,8 @@ if __name__ == '__main__':
     print(can_construct_memo("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"], {}))  # false
     print(can_construct_memo("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"], {}))  # true
     print(can_construct_memo("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", ["e", "eee", "eee", "eeee", "eeeee", "eeeeeee"], {}))  # false (there is no 'f' in the list)
+
+    print(can_construct_memo_idx(0, "abcdef", ["ab", "abc", "cd", "def", "abcd"], {}))  # true
+    print(can_construct_memo_idx(0, "skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"], {}))  # false
+    print(can_construct_memo_idx(0, "enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"], {}))  # true
+    print(can_construct_memo_idx(0, "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", ["e", "eee", "eee", "eeee", "eeeee", "eeeeeee"], {}))    # false (there is no 'f' in the list)
